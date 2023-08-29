b5946779944c050151529d95537cf03fb16390b6
Jeremy <jeremy.teurterie@gmail.com>
2023-08-29 11:33:11 +0700

    :sparkles: Feat: fetch notes through redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import ticketReducer from './features/ticketSlice';
+import noteReducer from './features/noteSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     tickets: ticketReducer,
+    notes: noteReducer,
   },
 });
