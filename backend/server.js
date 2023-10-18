b02315eb682c5e7cc4668c4d2b1f01a61bb24551
Jeremy <jeremy.teurterie@gmail.com>
2023-10-18 18:00:53 +0200

    :wrench: Configuration for production

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend')));
 
   app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    res.sendFile(__dirname, '../', 'frontend', 'index.html')
   );
 } else {
   app.get('/', (req, res) => {
