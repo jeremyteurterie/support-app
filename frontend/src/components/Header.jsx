f4d079193cbb96e509168d829048a9b34af875e6
Jeremy <jeremy.teurterie@gmail.com>
2023-10-28 17:23:07 +0200

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">Support App</Link>
+        <Link to="/">Support Center</Link>
       </div>
       <ul>
         {user ? (
